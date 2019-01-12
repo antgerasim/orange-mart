8b1fc85590b339d75430370bd3aaa2162384978f
agerasimov <agerasimov@WS52450545-DON>
2019-01-13 01:15:08 +0300

    Weiter mit Caching with cookie and localStorage 74%

---
+++ src/app/auth/cache.service.ts
@@ -0,0 +1,10 @@
+export abstract class CacheService {
+  protected getItem<T>(key: string): T {
+    const data = localStorage.getItem(key);
+    if(data && ){
+
+    }
+  }
+}
+
+//Weiter mit Caching with cookie and localStorage 74%
