e1d49bc77368eb244c3742f838db1f03eee55e36
antgerasim <antgerasim@gmail.com>
2019-01-13 17:28:26 +0300

    73% Fixed orangelogo in toolbar + added ngif  displayAccountIcons

--- src/app/auth/cache.service.ts
+++ src/app/auth/cache.service.ts
@@ -1,10 +1,13 @@
 export abstract class CacheService {
-  protected getItem<T>(key: string): T {
+
+
+/*   protected getItem<T>(key: string): T {
     const data = localStorage.getItem(key);
     if(data && ){
 
     }
-  }
+  } */
+
 }
 
 //Weiter mit Caching with cookie and localStorage 74%
